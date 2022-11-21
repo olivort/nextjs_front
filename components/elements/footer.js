eea15d01ee0a5e827fcf19617f9e2cc8e69996a1
olivort <44024113+olivort@users.noreply.github.com>
2022-11-21 13:20:22 +0000

    footer

--- components/elements/footer.js
+++ components/elements/footer.js
@@ -1,16 +1,15 @@
-//import PropTypes from "prop-types"
-//import { linkPropTypes, mediaPropTypes } from "utils/types"
-//import NextImage from "./image"
-//import CustomLink from "./custom-link"
+
+import NextImage from "./image"
+import CustomLink from "./custom-link"
 
 const Footer = ({ footer }) => {
   return (
     <footer className="pt-12 bg-gray-100">
       <div className="container flex flex-col lg:flex-row lg:justify-between">
         <div>
-          {/*footer.logo && (
+          {footer.logo && (
             <NextImage width="120" height="33" media={footer.logo} />
-          )*/}
+          )}
         </div>
         <nav className="flex flex-wrap flex-row lg:gap-20 items-start lg:justify-end mb-10">
           {footer.columns.map((footerColumn) => (
@@ -42,19 +41,4 @@ const Footer = ({ footer }) => {
   )
 }
 
-/*Footer.propTypes = {
-  footer: PropTypes.shape({
-    logo: mediaPropTypes.isRequired,
-    columns: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-          .isRequired,
-        title: PropTypes.string.isRequired,
-        links: PropTypes.arrayOf(linkPropTypes),
-      })
-    ),
-    smallText: PropTypes.string.isRequired,
-  }),
-}
-*/
 export default Footer
