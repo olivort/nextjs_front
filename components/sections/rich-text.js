208d78a0bbf73a3828efe4b98174e68ad29f2660
olivort <44024113+olivort@users.noreply.github.com>
2022-12-14 11:16:23 +0000

    adapted rich text to render html

--- components/sections/rich-text.js
+++ components/sections/rich-text.js
@@ -2,10 +2,16 @@
 import Markdown from "react-markdown"
 
 const RichText = ({ data, pageContext }) => {
+  const isMarkdown = true
   return (
+    <>
+    { isMarkdown ? 
     <div className="prose prose-lg container py-12">
       <Markdown>{data.content}</Markdown>
-    </div>
+    </div> 
+    : <div dangerouslySetInnerHTML={{ __html: data.content }}> </div>
+    }
+    </>
   )
 }
 
